1065fa06fb2aa6c67b2d0939960d1b4ca03da1c7
Jasmitha <168766728+jas127@users.noreply.github.com>
2024-11-04 20:53:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def load_images_from_directory(directory):
+    images = []
+    labels = []
+    for class_name in os.listdir(directory):
+        # print(f"class- name: {class_name}")
+        class_folder = os.path.join(directory, class_name)
+        if os.path.isdir(class_folder):
+            for filename in os.listdir(class_folder):
+                if filename.endswith(".png"):
+                    image_path = os.path.join(class_folder, filename)
+                    try:
+                        image = Image.open(image_path)
+                        image_array = np.array(image.resize((224, 224)))
+                        print(f"type np: {type(image_array)}")
+                        images.append(image_array)
+                        labels.append(class_name)
+                    except Exception as e:
+                        print(f"Error loading image {image_path}: {e}")
+
+    return images, labels
+
+
+x, y = load_images_from_directory("Infection Segmentation Data/Test/COVID-19")
+print(x)
+
+plt.imshow(x[5])
+# print(x[5].shape)
+plt.show()
